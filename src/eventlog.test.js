ed776538c931b77bc85b7cb219f70570da74ab7d
Cole Morton <cole.morton@hotmail.com>
2018-02-10 16:11:51 +0100

    add keyvalue

---
+++ src/eventlog.test.js
@@ -0,0 +1,30 @@
+const assert = require('assert')
+
+const IPFS = require('./ipfs')
+const OrbitDB = require('orbit-db')
+const eventlog = require('./eventlog')
+
+describe('eventlog', function() {
+  let ipfs
+
+  beforeEach(async function() {
+    ipfs = new IPFS()
+    await ipfs.onReady
+  })
+
+  it('should init', async function() {
+    try {
+      const orbitdb = new OrbitDB(ipfs, './orbitdb/examples/eventlog')
+      const db = await eventlog.init(orbitdb)
+      await eventlog.example(db)
+      await db.close()
+      await db.drop()
+      await orbitdb.stop()
+      await ipfs.stop()
+      process.exit(0)
+    } catch (e) {
+      console.error(e)
+      process.exit(1)
+    }
+  })
+})
---
+++ src/keyvalue.test.js
@@ -0,0 +1,30 @@
+const assert = require('assert')
+
+const IPFS = require('./ipfs')
+const OrbitDB = require('orbit-db')
+const keyvalue = require('./keyvalue')
+
+describe('keyvalue', function() {
+  let ipfs
+
+  beforeEach(async function() {
+    ipfs = new IPFS()
+    await ipfs.onReady
+  })
+
+  it('should init', async function() {
+    try {
+      const orbitdb = new OrbitDB(ipfs, './orbitdb/examples/keyvalue')
+      const db = await keyvalue.init(orbitdb)
+      await keyvalue.example(db)
+      await db.close()
+      await db.drop()
+      await orbitdb.stop()
+      await ipfs.stop()
+      process.exit(0)
+    } catch (e) {
+      console.error(e)
+      process.exit(1)
+    }
+  })
+})
